9ab4bb02e72a669e183b2622abf3f88c43d48508
openmetrue <openmetrue@gmail.com>
2023-04-23 23:16:39 +0800

    initial setup

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "GoogleCastSDK-beta",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "GoogleCastSDK-beta",
+            targets: ["GoogleCastSDK-beta"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "GoogleCastSDK-beta",
+            url: "https://dl.google.com/dl/chromecast/sdk/ios/GoogleCastSDK-ios-4.7.1_static_beta.xcframework.zip",
+            checksum: "7dbc206f6cf243694d4670d4e9ee4d03070781fd12b1e831b5a4f586d6c55f86"
+        )
+    ]
+)
